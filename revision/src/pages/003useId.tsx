dc3ee179f3b8e21999fc38d2bdb8f59c8f32200e
nkruger <nicolas.nkruger@gmail.com>
2023-05-02 16:51:18 -0300

    feat: :sparkles: useId

---
+++ revision/src/pages/003useId.tsx
@@ -0,0 +1,15 @@
+import { useId } from "react";
+export default function PasswordField() {
+  const passwordHintId = useId();
+  return (
+    <>
+      <label>
+        Password:
+        <input type="password" aria-describedby={passwordHintId} />
+      </label>
+      <p id={passwordHintId}>
+        The password should contain at least 18 characters
+      </p>
+    </>
+  );
+}
